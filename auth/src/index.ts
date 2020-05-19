bb483ef38d9a734dfe9bbc69aa9c32d655144fcc
bojanmitic <bojan.mitic@bilot.fi>
2020-05-19 19:51:10 +0300

    auth error handling set, router set, mongoose and coonecting to db

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,43 @@
-import express from "express";
-import { json } from "body-parser";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-foound-error';
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!");
-});
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!');
+  });
+};
+
+start();
